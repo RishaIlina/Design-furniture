7b7e5bfa1021ac93245aca5a718488db3d55ba4b
Ирина <brulichka@bk.ru>
2023-07-02 10:46:25 +0400

    Design-furniture-homework. Блок Последний шанс + адаптив

--- js/lib/main.js
+++ js/lib/main.js
@@ -1,3 +1,4 @@
+/* review */
 const swiper2 = new Swiper('.mySwiper', {
     slidesPerView: 1,
     spaceBetween: 500,
@@ -68,3 +69,29 @@ function mainSlider () {
 };
 
 mainSlider();
+
+
+
+
+/* tabs */
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelector('.tabs');
+  const tabsBtn = document.querySelectorAll('.tabs-btn');
+  const tabsContent = document.querySelectorAll('.tabs-content');
+
+  if (tabs) {
+    tabs.addEventListener('click', (e) => {
+      if (e.target.classList.contains('tabs-btn')) {
+        const tabsPath = e.target.dataset.tabsPath;
+        tabsBtn.forEach(el => el.classList.remove('tabs-btn__active'));
+        document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs-btn__active');
+        tabsHandler(tabsPath);
+      }
+    })
+  }
+
+  const tabsHandler = path => {
+    tabsContent.forEach(el => el.classList.remove('tabs-content__active'));
+    document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs-content__active');
+  }
+})
